6fc336c30068adc13e54acc7f015f3bc9d425cea
pedroddvo <pedroddvo@gmail.com>
2022-03-13 11:22:40 +0000

    cmake

---
+++ build/CMakeFiles/tetris.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetris.dir/src/main.cpp.obj"
+  "CMakeFiles/tetris.dir/src/main.cpp.obj.d"
+  "libtetris.dll.a"
+  "tetris.exe"
+  "tetris.exe.manifest"
+  "tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
